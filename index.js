ad9220cb30c4ed1bab27cb786c7a92264b20032f
Ramsey <browenbroz@gmail.com>
2024-04-18 15:01:39 +0300

    bwc3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    // This function will be executed when the DOM content has loaded
+    const textElement = document.getElementById('text');
+    textElement.textContent = 'This is really cool!';
+});
